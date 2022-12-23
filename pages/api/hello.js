f3209975da3d7ed9cb566e4940ef21c1c4e2c923
Canada Freelancer Ltd <34953799+canadafreelancer@users.noreply.github.com>
2022-12-23 12:54:45 -0800

    API page

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const express = require('express')
+const next = require('next')
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('/api/example', (req, res) => {
+    // Your API logic goes here
+    res.json({ message: 'Hello from the API' })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
