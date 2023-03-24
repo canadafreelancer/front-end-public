d10e081a722ed16657145df7b50789deecf4a4d4
fadipe benjamin <fadipemoyinoluwa2016@gmail.com>
2023-03-24 18:45:39 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  target: 'serverless', // add this line
+  output: 'export',
   images: {
     domains: ["source.unsplash.com"],
   },
   
 };
 
-module.exports = nextConfig;
+export default nextConfig;
